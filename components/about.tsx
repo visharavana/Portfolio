576cb84c4584f9e25da4f2ab62e4bea8b0744301
visharavana <vishal143muthu@gmail.com>
2024-12-17 23:31:24 +0530

    Build issue .

--- components/about.tsx
+++ components/about.tsx
@@ -4,11 +4,12 @@ const About = () => {
             <div className="container mx-auto px-6">
                 <h2 className="text-3xl font-bold mb-8 text-center">About Me</h2>
                 <p className="text-lg mb-4">
-                    I'm a data analyst with strong expertise in data visualization tools such as Apache Superset, Power BI, Metabase, and Preset.
+                    I&apos;m a data analyst with strong expertise in data visualization tools such as Apache Superset, Power BI, Metabase, and Preset.
                     My experience spans across various domains including procurement analysis, supply chain analysis, manufacturing execution system (MES) analysis, and finance analysis.
                 </p>
+
                 <p className="text-lg mb-4">
-                    I'm particularly passionate about procurement analysis and have developed several data analytics projects in this area.
+                    I&apos;m particularly passionate about procurement analysis and have developed several data analytics projects in this area.
                     I also have beginner-level knowledge in data engineering, including experience with data pipelines, Apache Airflow, and Mage AI orchestration tool.
                 </p>
                 <p className="text-lg">
